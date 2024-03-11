590bed3acc3da6febf6e646b8c38d4d1cd0f4bf3
philip schiøler <140426149+Pschioeler@users.noreply.github.com>
2024-03-11 13:15:12 +0100

    Revert "Merge pull request #6 from Pschioeler/LoginAndSignup"
    This reverts commit dede87f3f1cf8d902c5e7bf1302f28a2388746db, reversing
    changes made to 411d02cf522aee5586400ff08c5491993a9c4d81.

--- src/server.js
+++ src/server.js
@@ -1,40 +1,24 @@
 //Brug express
-const express = require('express');
+const express = require("express");
 //This is for JSON files
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
-const bodyParser = require('body-parser');
-const validatePassword = require('../src/Modules/passwordValidator');
-//modules:
-const quizController = require("./Modules/quizController");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 
-const corsOptions = {
-    origin: 'http://127.0.0.1:5500',
-    optionsSuccessStatus: 200 // nogle legacy browsere (IE11, forskellige SmartTVs) kan ikke håndtere 204 
-};
+//modules:
+const quizController = require("./Modules/quizController");
 
-app.use(cors(corsOptions));
+app.use(cors());
+
 //Body-parser til url encoded requests
 app.use(bodyParser.urlencoded({ extended: false }));
 //Body-parser til json requests
 app.use(bodyParser.json());
-app.use(express.json());
 
 //Lav endpoints her via app.get eller lignende
-app.post("/signup", (req, res) => {
-    const password = req.body.password;
-    console.log(password);
-    const validatePasswordStatus = validatePassword(password);
-    if (!validatePasswordStatus) {
-        console.log("Password does not meet requirements");
-        res.status(400).json({ error: 'Adgangskoden opfylder ikke kravene.' });
-    } else {
-        console.log("Password meets requirements");
-        res.send('Velkommen til');
-    }
-})
+
 // Indlæs quizzer ved opstart
 quizController.loadQuizzes();
 
@@ -49,5 +33,5 @@ app.get("/quiz/get-results", quizController.getResults);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
