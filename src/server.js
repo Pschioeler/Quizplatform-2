005129df743305574e46f9e619d08180f8a83e0d
philip schiøler <140426149+Pschioeler@users.noreply.github.com>
2024-03-11 09:25:37 +0100

    SessionID implemented
    Changes:
    
    Module: express-session  - downloaded
    Middleware: RequireAuthc - Opsat
    Basele routes som Dashboard / login er klargjort

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 //Brug express
 const express = require('express');
+// Bruges til Session ID
+const session = require('express-session');
 //This is for JSON files
 const cors = require('cors');
 const multer = require('multer');
@@ -10,12 +12,53 @@ const app = express();
 
 app.use(cors());
 
+app.use(session({
+    // secret kan/burde ændres til noget andet
+    secret: 'super-hemmelig-nøgle',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
 //Body-parser til url encoded requests
 app.use(bodyParser.urlencoded({ extended: false }));
 //Body-parser til json requests
 app.use(bodyParser.json());
 
 //Lav endpoints her via app.get eller lignende
+
+/*
+Middleware til authentication tjek
+Hver Route skal have en requireAuth i deres app.get
+*/
+const requireAuth = (req, res, next) => {
+    if (req.session.userId) {
+        next(); // User is authenticated, continue to next middleware
+    } else {
+        res.redirect('/login'); // User is not authenticated, redirect to login page
+    }
+}
+
+
+/* 
+Login route, sætter user id 
+*/
+app.post('/login', (req, res) => {
+    // Validate user credentials
+    if (validCredentials) {
+        req.session.userId = userId; // Set session identifier
+        res.redirect('/dashboard');
+    } else {
+        res.render('login', { error: 'Invalid username or password' });
+    }
+});
+
+/* 
+Dashbord route, sætter user id 
+*/
+app.get('/dashboard', requireAuth, (req, res) => {
+    // Render the dashboard page
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
