868ce37c4e18cc63eb7ff5a459f120b46e7117eb
philip schiøler <140426149+Pschioeler@users.noreply.github.com>
2024-03-11 13:14:15 +0100

    Revert "Merge branch 'Session-ID'"
    This reverts commit b271bc52fc3d429995e706c500e029936c82a4f4, reversing
    changes made to dede87f3f1cf8d902c5e7bf1302f28a2388746db.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 //Brug express
 const express = require('express');
-// Bruges til Session ID
-const session = require('express-session');
 //This is for JSON files
 const cors = require('cors');
 const multer = require('multer');
@@ -11,24 +9,13 @@ const validatePassword = require('../src/Modules/passwordValidator');
 //modules:
 const quizController = require("./Modules/quizController");
 const app = express();
-const fs = require("fs");
-//brug moduler ved: const myModule = require('./modules/myModule');
 
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500',
+    optionsSuccessStatus: 200 // nogle legacy browsere (IE11, forskellige SmartTVs) kan ikke håndtere 204 
+};
 
-// paths
-const usersFilePath = path.join(__dirname, "./DB/users.json");
-
-app.use(cors());
-
-app.use(session({
-    // secret kan/burde ændres til noget andet
-    secret: 'super-hemmelig-noegle',
-    cookie: { maxAge: 3600000 }, // gemmer session i 1 time
-    saveUninitialized: false,
-    resave: false,
-    //cookie: { secure: true }
-}));
-
+app.use(cors(corsOptions));
 //Body-parser til url encoded requests
 app.use(bodyParser.urlencoded({ extended: false }));
 //Body-parser til json requests
@@ -59,74 +46,6 @@ app.post("/quiz/submit-answer", quizController.submitAnswer);
 
 // Endpoint for at få resultaterne af en quiz
 app.get("/quiz/get-results", quizController.getResults);
-
-/*
-Middleware til authentication tjek
-Hver Route skal have en requireAuth i deres app.get
-*/
-const requireAuth = (req, res, next) => {
-    if (req.session.authenticated) {
-        next(); // User is authenticated, continue to next middleware
-    } else {
-        res.redirect('/login'); // User is not authenticated, redirect to login page
-    }
-}
-
-/* 
-Login route, sætter user id 
-*/
-app.post('/login', (req, res) => {
-    console.log(req.sessionID);
-    // tager et eventuelt username og password fra body
-    let { username, password } = req.body;
-    // Læs brugere fra DB
-    let users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
-    // find og tjek username og password
-    const user = users.find(
-        (u) => u.username === username && u.password === password
-    );
-        
-    if (!user) {
-        res.status(403).json({msg: 'Bad Credentials'});
-        res.end("Invalid Username");
-    } else {
-        req.session.authenticated = true;
-        req.session.user = user.username;
-        res.json(req.session);
-            if (user.isAdmin === true) {
-                //res.redirect("/adminpanel");
-            } else {
-                //res.redirect("/dashboard");
-            }
-    }
-});
-
-/* 
-Dashbord route
-*/
-app.get('/dashboard', requireAuth, (req, res) => {
-    // Render the dashboard page
-});
-
-/*
-Admin route
-*/
-app.get('/admin', requireAuth, (req, res) => {
-    // Render the admin page
-});
-
-// Logout
-app.get("/logout", (req, res) => {
-    req.session.destroy(function (err) {
-      if (err) {
-        console.log(err);
-        res.send("Error");
-      } else {
-        res.render("index", { title: "Login", logout: "Logout Succesfully!" });
-      }
-    });
-  });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
