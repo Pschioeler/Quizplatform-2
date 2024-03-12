ac9004d4f8a6eeaf7ad7e08301b24a57426109a0
radclim <mmajaaaa@gmail.com>
2024-03-12 17:52:08 +0100

    errorhandling

--- src/Modules/groups.js
+++ src/Modules/groups.js
@@ -6,11 +6,19 @@ function findUser(username) {
     const foundUser = users.find(user => {
         return bcrypt.compareSync(username, user.hashedUser);
     });
+    if (!foundUser) {
+        throw new Error(`Bruger '${username}' blev ikke fundet.`);
+    }
     return foundUser;
 }
 //funktion for at admins kan give adgang til grupper
 function allowGroupAccess(groupId, username) {
     const foundUser = findUser(username);
+    // Kontroller om brugeren allerede er tilmeldt til den givne gruppe
+    const existingGroup = foundUser.groups.find(group => group.groupId === groupId);
+    if (existingGroup) {
+        throw new Error(`Gruppen med ID '${groupId}' eksisterer allerede for bruger '${username}'.`);
+    }
     foundUser.groups.push({groupId: groupId, isAccepted: false});
     console.log(foundUser.groups);
 //Hvordan kan vi skrive den i JSON filen uden at skulle overwrite hele filen???-----------------------------------
@@ -18,11 +26,22 @@ function allowGroupAccess(groupId, username) {
 //funktion til at acceptere invitationer til grupper
 function acceptGroupInvitation(groupId, user) {
     const foundGroup = user.groups.find(group => group.hasOwnProperty("groupId") && group.groupId === groupId);
+    if (!foundGroup) {
+        throw new Error(`Gruppen med ID '${groupId}' blev ikke fundet.`);
+    }
     foundGroup.isAccepted = true;
 }
 //funktion se hvilke grupper brugeren er en del af
 function checkUserGroups(username) {
-    const user = findUser(username);
-    return user.groups;
+    try {
+        const user = findUser(username);
+        return user.groups;
+    } catch (error) {
+        console.error(error.message);
+        // returnerer tom array hvis der er ingen grupper
+        return [];
+    }
 }
+
+//funktion til at gemme dataen???-----------------------------------------
 //funktion displaye tilgængelige quizzer (baseret på medlemskab)
